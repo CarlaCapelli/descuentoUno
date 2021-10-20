07c2868d12c8829556dcb54cfbc01d299a08f831
CarlaCapelli <92063946+CarlaCapelli@users.noreply.github.com>
2021-10-19 21:08:49 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+let monto: number = 450.5;
+let descuento: number;
+console.log("el precio es ", monto);
+console.log("el descuento es del 10%");
+
+descuento = monto * 0.1;
+
+console.log("el precio con descuento es", descuento);
